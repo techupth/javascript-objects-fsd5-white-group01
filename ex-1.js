cd341c0afd25440231bfbdb795fc751cde4a8c1c
Pacharatnkp <pachara.pplzz@gmail.com>
2023-11-27 13:57:57 +0700

    Fix:object-ex

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder =  {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restaurantName: "MK",
+};
+Object.freeze(foodOrder);
+    
+foodOrder.totalPrice = 5000,
+foodOrder.paymentType = "Credit Card",
+
+console.log(foodOrder);
+
